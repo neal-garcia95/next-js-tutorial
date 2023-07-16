1c28f9f563d910f0c2ae8475a236685956f51d52
neal-garcia95 <neal@builtbymasonry.com>
2023-07-16 10:57:32 -0500

    Layout component and CSS Modules

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,12 +1,28 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Script from 'next/script';
+import Layout from '../../components/layout';
+
 
 export default function myFirstPost() {
     return(
-      <>
+      <Layout>
+       <Head>
+        <title>First Post </title>
+        <script src="https://connect.facebook.net/en_US/sdk.js" />
+        <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnload"
+          onLoad={()=>
+            console.log("script loaded correctly, window.FB has been populated")
+          }
+        >
+        </Script>
+      </Head>
       <h1> First Post</h1>
       <h2>
         <Link href="/">Back to Home</Link>
       </h2>
-      </>
+      </Layout>
     );
   }
